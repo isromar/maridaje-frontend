a641a55f5c47c7587e7dfcc973dbe188fe1ded4c
isromar <kaluca06@gmail.com>
2024-01-25 13:34:05 +0100

    Modificado la función ordenar de la tabla, ya que solamente ordenaba por tipo

--- src/components/tabla/index.jsx
+++ src/components/tabla/index.jsx
@@ -3,9 +3,8 @@ import { getData } from "../../utility/getData";
 import { apiUrl } from "../../data/Url";
 import { Eye, Trash2, Edit } from "react-feather";
 
+/* Este componente muestra una tabla de vinos y permite ordenar los vinos por nombre y tipo. */
 const TablaVinos = ({ busquedaNombreVino, selectedOption }) => {
-  console.log(busquedaNombreVino);
-  const [vinos, setVinos] = useState([]); // Inicializa vinos como un array
   const [orden, setOrden] = useState("asc"); // Estado para controlar el orden
   const [vinosFiltrados, setVinosFiltrados] = useState([]); // Estado para almacenar los vinos filtrados
 
@@ -18,7 +17,6 @@ const TablaVinos = ({ busquedaNombreVino, selectedOption }) => {
           selectedOption
         );
         const data = response["hydra:member"];
-        setVinos(data); // Actualiza el estado con los datos obtenidos
         setVinosFiltrados(data); // Inicializa los vinos filtrados con todos los vinos
       } catch (error) {
         console.error("Error al obtener los datos:", error);
@@ -28,17 +26,28 @@ const TablaVinos = ({ busquedaNombreVino, selectedOption }) => {
     fetchData();
   }, [busquedaNombreVino, selectedOption]);
 
-  // Función para manejar el clic en el encabezado "Tipo"
-  const handleSort = () => {
-  const nextOrder = orden === "asc" ? "desc" : "asc";
-  setOrden(nextOrder);
-  const sortedVinos = [...vinosFiltrados].sort((a, b) => {
-    if (nextOrder === "asc") {
-      return a.tipoVino.nombre.localeCompare(b.tipoVino.nombre);
-    } else {
-      return b.tipoVino.nombre.localeCompare(a.tipoVino.nombre);
+  // La función handleSort se encarga de cambiar el orden de los vinos según el encabezado en el que se haga clic.
+  const handleSort = (tipo) => {
+    const nextOrder = orden === "asc" ? "desc" : "asc";
+    setOrden(nextOrder);
+    let sortedVinos;
+    if (tipo === "Nombre") {  // Ordena los vinos por el nombre
+      sortedVinos = [...vinosFiltrados].sort((a, b) => {
+        if (nextOrder === "asc") {
+          return a.nombre.localeCompare(b.nombre);
+        } else {
+          return b.nombre.localeCompare(a.nombre);
+        }
+      });
+    } else if (tipo === "Tipo") { // Ordena los vinos por el tipo
+      sortedVinos = [...vinosFiltrados].sort((a, b) => {
+        if (nextOrder === "asc") {
+          return a.tipoVino.nombre.localeCompare(b.tipoVino.nombre);
+        } else {
+          return b.tipoVino.nombre.localeCompare(a.tipoVino.nombre);
+        }
+      });
     }
-  });
   setVinosFiltrados(sortedVinos); // Actualiza el estado con los vinos ordenados
   };
 
@@ -48,16 +57,13 @@ const TablaVinos = ({ busquedaNombreVino, selectedOption }) => {
         <table className="table table-striped tabla-vinos">
           <thead>
             <tr>
-              <th onClick={handleSort} className="cursor-pointer">
-                Nombre{orden === "asc" ? "⇅" : "⇅"}{" "}
-                {/* Agrega el símbolo de las flechas */}
+              <th onClick={() => handleSort("Nombre")} className="cursor-pointer">
+                Nombre{orden === "asc" ? " ⇅" : " ⇅"}{" "}
+                {/* Agrega el símbolo de las flechas y el manejador de clic para ordenar*/}
               </th>{" "}
-              {/* Agrega el manejador de clic para ordenar */}
-              <th onClick={handleSort} className="cursor-pointer">
-                Tipo{orden === "asc" ? "⇅" : "⇅"}{" "}
-                {/* Agrega el símbolo de las flechas */}
+              <th onClick={() => handleSort("Tipo")} className="cursor-pointer">
+                Tipo{orden === "asc" ? " ⇅" : " ⇅"}{" "}
               </th>{" "}
-              {/* Agrega el manejador de clic para ordenar */}
               <th>Maridaje</th>
               <th>Acciones</th>
             </tr>
