c78e16540ce1e2e6da532902d0af9b8e938dcbdf
isromar <kaluca06@gmail.com>
2024-02-03 15:37:23 +0100

    Ahora solo guarda en localStorage si el CIF es correcto

--- src/components/barra-navegacion/index.jsx
+++ src/components/barra-navegacion/index.jsx
@@ -8,30 +8,24 @@ const BarraNavegacion = () => {
   const [contrasena, setContrasena] = useState("");
 
   const handleUsuarioChange = (event) => {
-    setUsuario(event.target.value);
-    localStorage.setItem("usuario", event.target.value);
+      setUsuario(event.target.value);
   };
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    const data = await comprobarLogin(usuario);
+    const data = await comprobarLogin(usuario); // Llama a la función que comprueba si existe ese usuario en la bbdd 
     console.log(data);
     if (usuario === "" && (contrasena !== "" || contrasena === '')) {
     } else {
         if (data && data["hydra:member"].length > 0) {
-            console.log("22");
             const options = data["hydra:member"].map((bodega) => ({
             value: bodega.id,
             label: bodega.cif,
             name: bodega.nombre  // Obtener el nombre de la bodega
             }));
-            console.log("1");
-            console.log(options[0].label);
-            console.log("2");
-            console.log(usuario);
-
             if (options[0].label === usuario) {
             mostrarMensaje("Acceso correcto", `Te damos la bienvenida ${options[0].name}`, "success");
+            localStorage.setItem("usuario", usuario);
             setTimeout(() => {
                 // Cerrar el mensaje después de 3 segundos
                 Swal.close();
@@ -56,7 +50,7 @@ const BarraNavegacion = () => {
       <form onSubmit={handleFormSubmit}>
         <input
           type="text"
-          placeholder="Usuario"
+          placeholder={"Usuario"}
           value={usuario}
           onChange={handleUsuarioChange}
         />
--- src/utility/getData.js
+++ src/utility/getData.js
@@ -1,5 +1,6 @@
 import { apiUrl } from "../data/Url";
 
+/* Obtiene los datos según la url que se pasa por parámetro e incluye las opciones en la búsqueda en la bbdd si las hay */
 export const getData = async (url, busquedaNombreVino = '', selectedOption = null) => {
   if (busquedaNombreVino === '' && selectedOption === '') {
     return;
@@ -13,20 +14,21 @@ export const getData = async (url, busquedaNombreVino = '', selectedOption = nul
   }
 
   const urlConParametros = `${url}?${params.toString()}`;
-  const response = await fetch(urlConParametros);
+  const response = await fetch(urlConParametros); // Envía el parámetro con la url
   const data = await response.json();
   return data;
 }
 
+/* Comprueba si el usuario recibido está en la bbdd */
 export const comprobarLogin = async (usuario) => {
   let url = apiUrl.bodegas  // Url de la API con los registros de las bodegas
   if (usuario === '') {
     return;
   }
   const params = new URLSearchParams();
-  params.append('cif', usuario);
-  const urlConParametros = `${url}?${params.toString()}`; // Envía el parámetro usuario
-  const response = await fetch(urlConParametros);
+  params.append('cif', usuario);  // Añade el paráemtro 'cif' a la url
+  const urlConParametros = `${url}?${params.toString()}`;
+  const response = await fetch(urlConParametros); // Envía el parámetro con la url
   const data = await response.json();
   return data;
 }
