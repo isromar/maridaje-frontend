3c6cfb75eee7e6da471bc1941d43604c4969ce4e
isromar <kaluca06@gmail.com>
2024-04-30 10:24:27 +0200

    Haciendo editar vino Pone mensaje de error al acceder a una bodega que no es la autenticada

--- src/components/views/bodega/view/index.jsx
+++ src/components/views/bodega/view/index.jsx
@@ -4,18 +4,48 @@ import TopMenu from "../../../menu";
 import { useParams } from "react-router-dom";
 import { obtenerNombreBodega } from "../../../../utility/utils";
 import MenuElementosBodega from "../menu";
+import Swal from 'sweetalert2';
 
 const PerfilBodega = () => {
   const { bodegaId } = useParams();
   const [nombreBodega, setNombreBodega] = useState('');
+  const [acceso, setAcceso] = useState(true);
 
   useEffect(() => {
-    const fetchNombreBodega = async () => {
-      const nombre = await obtenerNombreBodega(bodegaId);
-      setNombreBodega(nombre);
+    const validarAccesoBodega = () => {
+      const bodegaIdLocalStorage = localStorage.getItem('bodegaId');
+      if (bodegaIdLocalStorage !== bodegaId) {
+        mostrarMensaje(
+          "Acceso denegado",
+          "No tienes acceso a esta bodega",
+          "error"
+        );
+        setAcceso(false);
+        setTimeout(() => {
+          window.location.href = `/perfil-bodega/${bodegaIdLocalStorage}`;
+        }, 4000);
+      } else {
+        const fetchNombreBodega = async () => {
+          const nombre = await obtenerNombreBodega(bodegaId);
+          setNombreBodega(nombre);
+        };
+        fetchNombreBodega();
+      }
     };
-    fetchNombreBodega();
-  }, []);
+
+    const mostrarMensaje = (titulo, mensaje, tipo) => {
+      Swal.fire({
+        title: titulo,
+        text: mensaje,
+        icon: tipo,
+        timer: 4000,
+        timerProgressBar: true,
+        showConfirmButton: false
+      });
+    };
+
+    validarAccesoBodega();
+  }, [bodegaId]);
 
   return (
     <div>
--- src/components/views/vino/edit/index.jsx
+++ src/components/views/vino/edit/index.jsx
@@ -1,16 +1,43 @@
 import React, { useState, useEffect } from "react"; // Importa useState y useEffect
 import { apiUrl } from "../../../../data/Url";
 import { useParams } from "react-router-dom";
-import Select from "react-select";
 import { mostrarMensaje } from "../../../../utility/utils";
 import Swal from "sweetalert2";
 import TopMenu from "../../../menu";
+import Select from "react-select";
 
 function EditarVino() {
-  let { vinoId } = useParams();
-  const [vino, setVino] = useState(null);
+  const { bodegaId } = useParams();
 
   const [tiposDeVino, setTiposDeVino] = useState([]);
+  const [denominacionOrigen, setDenominacionOrigen] = useState([]);
+  const opcionesVinoEcologico = [
+    { value: "si", label: "sí" },
+    { value: "no", label: "no" },
+  ];
+  const [comidas, setComidas] = useState([]);
+  const [nuevoVinoComidas, setNuevoVinoComidas] = useState([]);
+  const [hayComidasEnElInput, setHayComidasEnElInput] = useState(0);
+  const [variedadesUva, setVariedadesUva] = useState([]);
+  const [nuevoVinoVariedadUvas, setNuevoVinoVariedadUvas] = useState([]);
+  const [nuevoVino, setNuevoVino] = useState({
+    bodega: "",
+    nombre: "",
+    tipoVino: "",
+    maduracion: "",
+    ecologico: "",
+    precio: "",
+    denominacionOrigen: "",
+  });
+
+
+  
+
+  let { vinoId } = useParams();
+  const [vino, setVino] = useState(null);
+  const [editVino, setEditVino] = useState(null);
+
+ 
   const [tipoVinoSelected, setTipoVinoSelected] = useState("");
 
   const [denominacionOrigenOptions, setDenominacionOrigenOptions] = useState([]);
@@ -20,17 +47,181 @@ function EditarVino() {
   const [vinoPrecio, setVinoPrecio] = useState([]);
   const [vinoPrecioSelected, setVinoPrecioSelected] = useState("");
 
-  const [comidas, setComidas] = useState("");
+
   const [comidaSelected, setComidaSelected] = useState("");
 
   const [vinoEcologicoSelected, setVinoEcologicoSelected] = useState("");
-  const opcionesVinoEcologico = [
-    { value: "si", label: "sí" },
-    { value: "no", label: "no" },
-  ];
-  const [variedadesUva, setVariedadesUva] = useState([]);
+
   const [variedadUvaSelected, setVariedadUvaSelected] =
     useState([]);
+
+
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchData = async () => {
+        mostrarMensaje(
+            "Cargando datos...",
+            "Espere mientras se cargan los datos",
+            "info"
+        );
+
+        await Promise.all([
+            fetchTiposDeVino(),
+            fetchDenominacionDeOrigen(),
+            fetchComidas(),
+            fetchVariedadesUva()
+        ]);
+
+        if (isMounted) {
+            // Cerrar el mensaje después de completar todas las solicitudes
+            Swal.close();
+        }
+    };
+
+    fetchData();
+
+    return () => {
+        isMounted = false;
+    };
+}, []);
+
+  useEffect(() => {
+  }, [nuevoVino]);
+
+  const fetchDenominacionDeOrigen = async () => {
+    try {
+      const response = await fetch(apiUrl.denominacionDeOrigen);
+      const data = await response.json();
+      if (data && data["hydra:member"]) {
+        const options = data["hydra:member"]
+          .map((denominacion) => ({
+            value: denominacion.id,
+            label: denominacion.nombre,
+          }))
+          .sort((a, b) => a.label.localeCompare(b.label));
+        setDenominacionOrigen(options);
+
+        Swal.close();
+      } else {
+        console.error("No se encontraron datos.");
+      }
+    } catch (error) {
+      console.error("Error al obtener los datos:", error);
+
+      // Mostrar mensaje de error
+      mostrarMensaje(
+        "Error al cargar los datos",
+        "Ha ocurrido un error al cargar los datos",
+        "error"
+      );
+    }
+  };
+
+  const handleChange = (selectedOption, stateName) => {
+    if (stateName === "tipoVinoSelected") {
+      setNuevoVino({ ...nuevoVino, tipoVino: selectedOption });
+    } else if (stateName === "denominacionOrigenSelected") {
+      setNuevoVino({
+        ...nuevoVino,
+        denominacionOrigen: selectedOption,
+      });
+    } else if (stateName === "vinoEcologicoSelected") {
+      setNuevoVino({
+        ...nuevoVino,
+        ecologico: selectedOption.value,
+      });
+    } else if (stateName === "comidasSelected") {
+      setHayComidasEnElInput(1);
+      setNuevoVinoComidas(selectedOption.map((option) => option.value));
+    } else if (stateName === "variedadUvaSelected") {
+      setNuevoVinoVariedadUvas(selectedOption.map((option) => option.value));
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    if (!nuevoVino.nombre || !nuevoVino.tipoVino || hayComidasEnElInput === 0) {
+      mostrarMensaje(
+        "Campos obligatorios",
+        "Por favor, rellena los campos Nombre, Tipo de Vino y Marida con",
+        "warning",
+        4000
+      );
+      event.preventDefault();
+      return;
+    }
+    event.preventDefault();
+
+    const bodegaIri = `${apiUrl.bodegas}/${bodegaId}`;
+    const tipoVinoIri = `${apiUrl.tiposDeVino}/${nuevoVino.tipoVino.value}`;
+    let denominacionOrigenIri = null;
+    if (nuevoVino.denominacionOrigen && nuevoVino.denominacionOrigen.value) {
+      denominacionOrigenIri = `${apiUrl.denominacionDeOrigen}/${nuevoVino.denominacionOrigen.value}`;
+    }
+    const comidasIris = nuevoVinoComidas.map(
+      (comidaId) => `${apiUrl.comidas}/${comidaId}`
+    );
+    const variedadUvasIris = nuevoVinoVariedadUvas.map(
+      (variedadUvaId) => `${apiUrl.variedadesUva}/${variedadUvaId}`
+    );
+
+    const nuevoVinoCompleto = {
+      ...nuevoVino,
+      bodega: bodegaIri,
+      nombre: nuevoVino.nombre,
+      tipoVino: tipoVinoIri,
+      maduracion: nuevoVino.maduracion,
+      precio: parseFloat(nuevoVino.precio),
+      denominacionOrigen: denominacionOrigenIri,
+      ecologico: nuevoVino.ecologico === "si" ? true : false,
+      comida: comidasIris,
+      variedadUva: variedadUvasIris,
+    };
+    console.log(nuevoVinoCompleto);
+
+    try {
+      const response = await fetch(apiUrl.vinos, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/ld+json",
+        },
+        body: JSON.stringify(nuevoVinoCompleto),
+      });
+
+      if (response.ok) {
+        // Crear las relaciones entre el vino y las comidas seleccionadas
+        const data = await response.json();
+        //const vinoId = data["@id"].split("/").pop();
+
+        mostrarMensaje(
+          "Vino creado",
+          "El vino se ha creado con éxito",
+          "success"
+        )
+        .then(data => console.log(data))
+
+        // Restablecer el formulario después de la creación exitosa
+        setNuevoVino({
+          nombre: "",
+          tipoVino: "",
+          maduracion: "",
+          ecologico: "no",
+          precio: 0,
+          denominacionOrigen: null,
+        });
+
+
+      } else {
+        mostrarMensaje("Error", "Hubo un error al crear el vino", "error");
+      }
+    } catch (error) {
+      mostrarMensaje("Error", "Hubo un error al crear el vino", "error");
+    }
+  };
+
+
+
 
   const fetchTiposDeVino = async () => {
     try {
@@ -112,9 +303,12 @@ function EditarVino() {
     if (vinoId) {
       fetch(`${apiUrl.vinos}/${vinoId}`)
         .then((response) => response.json())
-        .then((data) => setVino(data));
+        .then((data) => {
+          setVino(data);
+          console.log(data);
+        });
     }
-  }, []);
+}, []);
 
   useEffect(() => {
     fetchTiposDeVino();
@@ -126,19 +320,6 @@ function EditarVino() {
   if (!vino) {
     return <div>Cargando...</div>;
   }
-
-  const handleChange = (selectedOption, stateName) => {
-
-    if (stateName === "tipoVinoSelected") {
-      setTipoVinoSelected(selectedOption);
-    } else if (stateName === "denominacionOrigenSelected") {
-      setDenominacionOrigenSelected(selectedOption);
-    } else if (stateName === "vinoEcologicoSelected") {
-      setVinoEcologicoSelected(selectedOption);
-    } else if (stateName === "variedadUvaSelected") {
-        setVariedadUvaSelected(selectedOption);
-    }
-  };
 
   const handleInputChange = (event, key) => {
     const value = event.target.value;
@@ -192,113 +373,145 @@ function EditarVino() {
     }
   };
 
+
+
+
   return (
     <div>
-    <div>
-      <TopMenu/>
-    </div>
-    <form className="view">
-      <section className="vino-view">
-        <h2 className="centrar">
-          <span>Vino - {vino.nombre} </span>
-        </h2>
-        <button className="btn-editar-vino btn btn-light" onClick={saveData}>Guardar</button>
-        <table className="tabla-view">
-          <tr>
-            <td>Tipo de vino:</td>
-            <td>
+      <div>
+        <TopMenu />
+      </div>
+      <form className="view" id="form-nuevo-vino" onSubmit={handleSubmit}>
+        <section className="view">
+          <h2 className="centrar">Editar Vino - {vino.nombre}</h2>
+          <button type="submit" className="btn btn-light button-guardar">
+              Guardar vino
+            </button>
+
+          <table className="tabla-view">
+
+            <tr>
+              <td>Nombre:</td>
+              <td>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={nuevoVino.nombre}
+                  onChange={(e) =>
+                    setNuevoVino({ ...nuevoVino, nombre: e.target.value })
+                  }
+                  placeholder={vino.nombre}
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tipo de vino:</td>
+              <td>
+                <Select
+                  type="text"
+                  placeholder={vino.tipoVino.nombre}
+                  value={nuevoVino.tipoVino}
+                  onChange={(tipoVinoSelected) =>
+                    handleChange(tipoVinoSelected, "tipoVinoSelected")
+                  }
+                  options={tiposDeVino}
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>Maduración:</td>
+              <td>
+                <input
+                  type="text"
+                  placeholder={vino.maduracion || ""}              className="form-control"
+                  value={nuevoVino.maduracion}
+                  onChange={(e) =>
+                    setNuevoVino({ ...nuevoVino, maduracion: e.target.value })
+                  }
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>Denominación de origen:</td>
+              <td>
+                <Select
+                  type="text"
+                  placeholder={vino.denominacionOrigen.nombre || ""}
+                  value={nuevoVino.denominacionOrigen}
+                  onChange={(denominacionOrigenSelected) =>
+                    handleChange(
+                      denominacionOrigenSelected,
+                      "denominacionOrigenSelected"
+                    )
+                  }
+                  options={denominacionOrigen}
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>Ecológico:</td>
+              <td>
+                <Select
+                  type="text"
+                  placeholder={vino.ecologico}
+                  value={opcionesVinoEcologico.find(
+                    (opcion) => opcion.value === nuevoVino.ecologico
+                  )}
+                  onChange={(selectedOption) =>
+                    setNuevoVino({
+                      ...nuevoVino,
+                      ecologico: selectedOption.value,
+                    })
+                  }
+                  options={opcionesVinoEcologico}
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>Precio:</td>
+              <td>
+                <input
+                  type="text"
+                  placeholder={vino.precio || 0}
+                  className="form-control"
+                  value={nuevoVino.precio}
+                  onChange={(e) =>
+                    setNuevoVino({ ...nuevoVino, precio: e.target.value })
+                  }
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>Marida con:</td>
+              <td>
+                <Select
+                  isMulti
+                  value={vino.comida.map(comida => ({ value: comida.id, label: comida.nombre }))}
+                  onChange={(selectedOptions) => setComidaSelected(selectedOptions)}
+                  options={comidas}
+                  placeholder={vino.comida && vino.comida.length > 0
+                    ? [
+                        {
+                          value: vino.comida[0].id,
+                          label: vino.comida[0].nombre
+                        }
+                      ]
+                    : ""
+                  }
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>Variedad de uva:</td>
+              <td>
               <Select
-                value={tipoVinoSelected}
-                onChange={(tipoVinoSelected) =>
-                  handleChange(tipoVinoSelected, "tipoVinoSelected")
-                }
-                options={tiposDeVino}
-                placeholder={vino.tipoVino.nombre}
-              />
-            </td>
-          </tr>
-
-          <tr>
-            <td>Maduración:</td>
-            <td>
-              <input
-                type="text"
-                className="form-control "
-                value={vino.maduracion}
-                onChange={(event) => handleInputChange(event, 'maduracion')}
-              />
-            </td>
-          </tr>
-
-          <tr>
-            <td>Denominación de origen:</td>
-            <td>
-              <Select
-                value={denominacionOrigenSelected || ""}
-                onChange={(denominacionOrigenSelected) =>
-                  handleChange(
-                    denominacionOrigenSelected,
-                    "denominacionOrigenSelected"
-                  )
-                }
-                options={denominacionOrigenOptions}
-                placeholder={vino.denominacionOrigen.nombre || ""}
-              />
-            </td>
-          </tr>
-
-          <tr>
-            <td>Ecológico:</td>
-            <td>
-              <Select
-                value={vinoEcologicoSelected}
-                onChange={(vinoEcologicoSelected) =>
-                  handleChange(vinoEcologicoSelected, "vinoEcologicoSelected")
-                }
-                options={opcionesVinoEcologico}
-                placeholder={vino.ecologico}
-              />
-            </td>
-          </tr>
-
-          <tr>
-            <td>Precio:</td>
-            <td>
-              <input
-                type="text"
-                className="form-control"
-                value={`${vino.precio} €`}
-                onChange={(event) => handleInputChange(event, 'precio')}
-              />
-            </td>
-          </tr>
-
-
-          <tr>
-            <td>Marida con:</td>
-            <td>
-              <Select
-                isMulti
-                value={vino.comida.map(comida => ({ value: comida.id, label: comida.nombre }))}
-                onChange={(selectedOptions) => setComidaSelected(selectedOptions)}
-                options={comidas}
-                placeholder={vino.comida && vino.comida.length > 0
-                  ? [
-                      {
-                        value: vino.comida[0].id,
-                        label: vino.comida[0].nombre
-                      }
-                    ]
-                  : ""
-                }
-              />
-            </td>
-          </tr>
-
-          <tr>
-            <td>Variedad de uva:</td>
-            <td>
-            <Select
                 isMulti
                 value={vino.variedad_uva.map(uva => ({ value: uva.id, label: uva.nombre }))}
                 onChange={(selectedOptions) =>
@@ -316,63 +529,11 @@ function EditarVino() {
                   : ""
               }
               />
-            </td>
-          </tr>
-
-        </table>
-      </section>
-
-      <section className="bodega-view">
-        <h4 className="centrar">
-          <span>Bodega - {vino.bodega.nombre} </span>
-        </h4>
-        <table className="tabla-view">
-          <tr>
-            <td>Dirección:</td>
-            <td>
-              <input
-                type="text"
-                className="form-control "
-                value={vino.bodega.direccion}
-              />
-            </td>
-          </tr>
-
-          <tr>
-            <td>Teléfono:</td>
-            <td>
-              <input
-                type="text"
-                className="form-control "
-                value={vino.bodega.telefono}
-              />
-            </td>
-          </tr>
-
-          <tr>
-            <td>CIF:</td>
-            <td>
-              <input
-                type="text"
-                className="form-control "
-                value={vino.bodega.cif}
-              />
-            </td>
-          </tr>
-
-          <tr>
-            <td>Web:</td>
-            <td>
-              <input
-                type="text"
-                className="form-control "
-                value={vino.bodega.web}
-              />
-            </td>
-          </tr>
-        </table>
-      </section>
-    </form>
+              </td>
+            </tr>
+          </table>
+        </section>
+      </form>
     </div>
   );
 }
