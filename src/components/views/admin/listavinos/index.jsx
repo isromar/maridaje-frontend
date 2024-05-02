fc34b8a751ac182a6e2289230a47da311d2df74e
isromar <kaluca06@gmail.com>
2024-05-02 16:31:23 +0200

    Mensaje para cargar los datos

--- src/components/views/admin/listavinos/index.jsx
+++ src/components/views/admin/listavinos/index.jsx
@@ -2,13 +2,24 @@ import React, { useState, useEffect } from "react";
 import TopMenu from "../../../menu";
 
 import {
-  mostrarMensaje,
-  mostrarMensajeConfirmacion,
+  mostrarMensaje
 } from "../../../../utility/utils";
 import Swal from "sweetalert2";
 import TablaVinos from "../../vino/list";
 
+
 const ListaVinosAdmin = () => {
+
+  useEffect(() => {
+    mostrarMensaje("Cargando datos...", "Espere mientras se cargan los datos", "info");
+
+    // Simulación de carga de datos
+    setTimeout(() => {
+      Swal.close();
+
+    }, 4000); // Simular una carga de 2 segundos
+  }, []);
+
   return (
     <div className="perfil-admin-container">
       <div>
@@ -21,8 +32,8 @@ const ListaVinosAdmin = () => {
             <span>Administrador</span>
           </h2>
         </section>
-            <TablaVinos/>
-      </div>
+          <TablaVinos/>
+        </div>
     </div>
   );
 };
